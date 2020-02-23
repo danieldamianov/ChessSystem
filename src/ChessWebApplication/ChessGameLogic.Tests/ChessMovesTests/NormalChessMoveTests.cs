e547927394d997235cebabdf77b45c8b256cbab2
danieldamianov <danieldamianov02@gmail.com>
2020-02-23 14:11:28 +0200

    Added  NormalChessMovePositions and tests for it.

---
+++ src/ChessWebApplication/ChessGameLogic.Tests/ChessMovesTests/NormalChessMoveTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ChessGameLogic.Tests.ChessMovesTests
+{
+    [TestFixture]
+    public class NormalChessMoveTests
+    {
+        [Test]
+        [TestCase('a',1,'b',2,true)]
+        [TestCase('*',1,'b',2,false)]
+        [TestCase('t',0,'b',2,false)]
+        [TestCase('a',1,'l',2,false)]
+        [TestCase('a',1,'b',123,false)]
+        public void TestNormalChessMoveSettingPositionsCorrect(char initialPositionHorizontal, int initialPositionVertical
+                , char targetPositionHorizontal, int targetPositionVertical,bool isValid)
+        {
+            var normalChessMoveType = ChessGameLogicProvider.GetType("ChessGameLogic.ChessMoves.NormalChessMovePositions");
+            var chessBoardPositionType = ChessGameLogicProvider.GetType("ChessGameLogic.ChessBoardPosition");
+
+            if (isValid == false)
+            {
+                Assert.That(() => normalChessMoveType.GetConstructors(BindingFlags.Instance
+                    | BindingFlags.NonPublic)[0].Invoke(new object[] { initialPositionHorizontal,initialPositionVertical
+                , targetPositionHorizontal, targetPositionVertical }),
+                        Throws.InnerException.InstanceOf(ChessGameLogicProvider.GetType("ChessGameLogic.Exceptions.PositionOutOfBoardException")));
+            }
+            else
+            {
+                var normalChessMoveInstance = normalChessMoveType.GetConstructors(BindingFlags.Instance
+                    | BindingFlags.NonPublic)[0].Invoke(new object[] { initialPositionHorizontal,initialPositionVertical
+                , targetPositionHorizontal, targetPositionVertical});
+
+                var initialPosition = normalChessMoveType
+                    .GetProperty("InitialPosition", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(normalChessMoveInstance);
+                var targetPosition = normalChessMoveType
+                    .GetProperty("TargetPosition", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(normalChessMoveInstance);
+
+                var chessBoardPositionEqualsMethod = chessBoardPositionType.GetMethods()
+                .Where(m => FilterMethod(m))
+                .First();
+
+                Assert.That((bool)chessBoardPositionEqualsMethod.Invoke(initialPosition, new object[] {
+                chessBoardPositionType.GetConstructors(BindingFlags.Instance
+                    | BindingFlags.NonPublic)[0].Invoke(new object[] {initialPositionHorizontal,initialPositionVertical })}));
+
+                Assert.That((bool)chessBoardPositionEqualsMethod.Invoke(targetPosition, new object[] {
+                chessBoardPositionType.GetConstructors(BindingFlags.Instance
+                    | BindingFlags.NonPublic)[0].Invoke(new object[] { targetPositionHorizontal, targetPositionVertical })}));
+            }
+        }
+        private bool FilterMethod(MethodInfo m)
+        {
+            ParameterInfo[] parameters = m.GetParameters();
+            return m.Name == "Equals" && parameters.Length == 1
+                            && parameters[0].ParameterType == m.DeclaringType;
+        }
+    }
+}
---
+++ src/ChessWebApplication/ChessGameLogic/ChessMoves/NormalChessMovePositions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGameLogic.ChessMoves
+{
+    internal class NormalChessMovePositions
+    {
+        private ChessBoardPosition initialPosition;
+
+        private ChessBoardPosition targetPosition;
+
+        internal NormalChessMovePositions(char initialPositionHorizontal, int initialPositionVertical
+                , char targetPositionHorizontal, int targetPositionVertical)
+        {
+            this.InitialPosition = new ChessBoardPosition(initialPositionHorizontal, initialPositionVertical);
+            this.TargetPosition = new ChessBoardPosition(targetPositionHorizontal, targetPositionVertical);
+        }
+
+        internal ChessBoardPosition InitialPosition
+        {
+            get { return initialPosition; }
+            private set { initialPosition = value; }
+        }
+
+        internal ChessBoardPosition TargetPosition
+        {
+            get { return targetPosition; }
+            private set { targetPosition = value; }
+        }
+    }
+}
