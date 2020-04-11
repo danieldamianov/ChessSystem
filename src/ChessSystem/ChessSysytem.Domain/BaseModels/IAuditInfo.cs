5b5e3a420dc18ab4eafb5403c901dc6f80fe6915
danieldamianov <danieldamianov02@gmail.com>
2020-04-11 15:11:37 +0300

    Added BaseModels folder and entities inside it.

---
+++ src/ChessSystem/ChessSysytem.Domain/BaseModels/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessSystem.Domain.BaseModels
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
