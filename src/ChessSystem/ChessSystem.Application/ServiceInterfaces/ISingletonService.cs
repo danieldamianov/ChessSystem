ce094914fcf9f5890909c7db26a90976f325caf6
danieldamianov <danieldamianov02@gmail.com>
2020-04-11 23:48:50 +0300

    Added Result class and Service Types.

---
+++ src/ChessSystem/ChessSystem.Application/ServiceInterfaces/ISingletonService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessSystem.Application.ServiceInterfaces
+{
+    public interface ISingletonService
+    {
+    }
+}
