a02a87d1785e2002c877109f0aa21d2a0241acbb
danieldamianov <danieldamianov02@gmail.com>
2020-04-28 14:12:03 +0300

    Enabled null values for EndGameInfo.

---
+++ src/ChessSystem/Infrastructure/Migrations/20200428103432_EnabledNullValuesForEndGameInfoProperty..cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class EnabledNullValuesForEndGameInfoProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EndGameInfo",
+                table: "ChessGames",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EndGameInfo",
+                table: "ChessGames",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
